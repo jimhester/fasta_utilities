0a7602383c9de390cfcb4a32c228c0fcb36dac84
Jim Hester <james.f.hester@gmail.com>
2013-04-04 09:30:33 -0400

    Fix mpileup_counts raf calcuation
    Use total instead of coverage

--- scripts/mpileup_counts.pl
+++ scripts/mpileup_counts.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use autodie;
 ###############################################################################
 # Title:mpileup_counts.pl
 # Created: 2012 Oct 26 08:28:19 AM
-# Modified: 2013 Mar 25 02:16:11 PM
+# Modified: 2013 Apr 04 09:30:28 AM
 # Purpose:parses a mpileup file and gets the basecounts
 # By Jim Hester
 ###############################################################################
@@ -14,17 +15,21 @@ use strict;
 ###############################################################################
 use Getopt::Long;
 use Pod::Usage;
-my $man = 0;
+my $man  = 0;
 my $help = 0;
-GetOptions('help|?' => \$help, man => \$man) or pod2usage(2);
+GetOptions( 'help|?' => \$help, man => \$man ) or pod2usage(2);
 pod2usage(2) if $help;
-pod2usage(-verbose => 2) if $man;
-pod2usage("$0: No files given.")  if ((@ARGV == 0) && (-t STDIN));
+pod2usage( -verbose => 2 ) if $man;
+pod2usage("$0: No files given.") if ( ( @ARGV == 0 ) && ( -t STDIN ) );
 
 ###############################################################################
 # Automatically extract compressed files
 ###############################################################################
-@ARGV = map { s/(.*\.gz)\s*$/pigz -dc < $1|/; s/(.*\.bz2)\s*$/pbzip2 -dc < $1|/;$_ } @ARGV;
+@ARGV = map {
+  s/(.*\.gz)\s*$/pigz -dc < $1|/;
+  s/(.*\.bz2)\s*$/pbzip2 -dc < $1|/;
+  $_
+} @ARGV;
 ###############################################################################
 # mpileup_counts.pl
 ###############################################################################
@@ -32,41 +37,49 @@ use Carp;
 
 #setup and print headers
 my @bases = qw(A C G T N);
-print join("\t","chromosome","position","coverage","reference","raf",@bases)."\n";
+print join( "\t",
+            "chromosome", "position", "coverage", "deleted", "reference",
+            "raf", @bases )
+  . "\n";
 
 my %sequences;
-while(<>){
+while (<>) {
+
   #parse mpileup
-  my($chr,$pos,$ref,$coverage,$reads,$qualities) = split;
-  $sequences{$chr}{last} = 0 unless exists $sequences{$chr}; # initialize last position
-  $reads = uc $reads; #make all letters uppercase
-  $reads =~ s/[.,]/$ref/g; #change ref bases to ref letter
-  $reads =~ s/\^.//g; #remove read starts and mapping quality
-  $reads =~ s/\$//g; #remove read ends
+  my ( $chr, $pos, $ref, $coverage, $reads, $qualities ) = split;
+  $sequences{$chr}{last} = 0
+    unless exists $sequences{$chr};    # initialize last position
+  my $starts = $reads =~ s/\^.//g;     #remove read starts and mapping quality
+  my $ends   = $reads =~ s/\$//g;      #remove read ends
+  my $indels = $reads =~ s/[\+\-]([0-9]+)(??{"\\w{$^N}"})//g;   #remove indels
+  my $skip    = $reads =~ s/[<>]//g;    #remove reference skips
   my $deleted = $reads =~ s/[*]//g;
-  $reads =~ s/[\+\-]([0-9]+)(??{"\\w{$^N}"})//g; #remove indels
+  $reads = uc $reads;                   #make all letters uppercase
+  $reads =~ s/[.,]/$ref/g;              #change ref bases to ref letter
 
   #get counts for bases
   my %counts;
   my $total;
-  for my $base(@bases){
-    my $count = $reads =~ s/$base/$base/g || 0;
-    $counts{$base}= $count ? $count : 0;
-    $total+=$count;
+  for my $base (@bases) {
+    my $count = $reads =~ s/$base/$base/g;
+    $counts{$base} = $count ? $count : 0;
+    $total += $count;
   }
-  croak "$total is not equal to $coverage\n" if $total != $coverage - $deleted;
+  $total += $skip + $deleted;
+  if ( $total != $coverage ) {
+    croak sprintf
+      "starts:%i ends:%i indels:%i skip:%i deleted:%i $total is not equal to $coverage on the following line\n$_",
+      $starts, $ends, $indels, $skip, $deleted;
+  }
+
   #print data
-  print join("\t", $chr, $pos, $coverage, $ref,sprintf("%2f", $counts{$ref}/$coverage), map { $counts{$_} } @bases), "\n";
+  print join( "\t",
+              $chr, $pos, $coverage, $deleted, $ref,
+              sprintf( "%2f", $counts{$ref} / $total ),
+              map { $counts{$_} } @bases ),
+    "\n";
 }
 
-open my $out, "| ~/fu/wrap.pl" or die "$!: Could not open file\n";
-for my $name (keys %sequences){
-  print $out ">$name\n$sequences{$name}\n";
-}
-sub most_frequent_base{
-  my($hash_ref) = @_;
-  return (sort { $hash_ref->{$b} <=> $hash_ref->{$a} } keys %{ $hash_ref })[0];
-}
 ###############################################################################
 # Help Documentation
 ###############################################################################
