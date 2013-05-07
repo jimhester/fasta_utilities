a6bbb652378c798058d55f5c695f414f1da8ad90
Jim Hester <james.f.hester@gmail.com>
2013-05-07 13:49:09 -0400

    Use full path in prefix

--- scripts/split_fasta.pl
+++ scripts/split_fasta.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use autodie qw(:all);
 use warnings;
 use strict;
 ###############################################################################
@@ -13,34 +14,46 @@ use strict;
 ###############################################################################
 use Getopt::Long;
 use Pod::Usage;
-my $man = 0;
+my $man  = 0;
 my $help = 0;
 my $prefix;
-my $suffix='.fa';
+my $suffix = '.fa';
+my $amount;
 my $num;
 my $width;
 my $force;
-GetOptions('width=i' => \$width, 'suffix=s' => \$suffix, 'num=i' => \$num, 
-  'prefix=s' => \$prefix, 'force' => \$force, 'help|?' => \$help, man => \$man)
+GetOptions( 'width=i'  => \$width,
+            'suffix=s' => \$suffix,
+            'amount=i' => \$amount,
+            'num=i'    => \$num,
+            'prefix=s' => \$prefix,
+            'force'    => \$force,
+            'help|?'   => \$help,
+            man        => \$man )
   or pod2usage(2);
 pod2usage(2) if $help;
-pod2usage(-verbose => 2) if $man;
-pod2usage("$0: No files given.")  if ((@ARGV == 0) && (-t STDIN));
+pod2usage( -verbose => 2 ) if $man;
+pod2usage("$0: No files given.") if ( ( @ARGV == 0 ) && ( -t STDIN ) );
 
 ###############################################################################
 # Automatically extract compressed files
 ###############################################################################
-@ARGV = map { s/(.*\.gz)\s*$/pigz -dc < $1|/; s/(.*\.bz2)\s*$/pbzip2 -dc < $1|/;$_ } @ARGV;
+@ARGV = map {
+  s/(.*\.gz)\s*$/pigz -dc < $1|/;
+  s/(.*\.bz2)\s*$/pbzip2 -dc < $1|/;
+  $_
+} @ARGV;
 ###############################################################################
 # split_fasta.pl
 ###############################################################################
 
-if(not $prefix){
-  if(-t STDIN){
-    ($prefix) = $ARGV[0] =~ m!.*?/*([^/.]+)\.*!;
-    $prefix.='_';
-  } else {
-    $prefix='';
+if ( not $prefix ) {
+  if ( -t STDIN ) {
+    ($prefix) = remove_suffix($ARGV[0]);
+    $prefix .= '_';
+  }
+  else {
+    $prefix = '';
   }
 }
 
@@ -49,41 +62,73 @@ use ReadFastx;
 my $fastx = ReadFastx->new();
 my $OUT;
 
-if($num){
+if ($num) {
   my @seqs;
-  my $size=0;
-  while(my $rec = $fastx->next_seq){
-    push @seqs,$rec;
-    $size+=length($rec->sequence);
-  }
-  my $split_size = $size/$num;
-  my $cur_size=0;
-  my $cutoff = 0;
-  my $itr=0;
-  for my $seq(@seqs){
-    if($cur_size >= $cutoff){
-      open_file("$prefix$itr$suffix");
+  my $count = 0;
+  my $itr   = 0;
+  while ( my $rec = $fastx->next_seq ) {
+    if ( $count > $num ) {
+      print_array("$prefix$itr$suffix", \@seqs);
+      @seqs  = ();
+      $count = 0;
       $itr++;
-      $cutoff+=$split_size;
     }
-    $seq->print(fh => $OUT, width => $width);
-    $cur_size+=length($seq->sequence);
+    push @seqs, $rec;
+    $count++;
   }
-} else {
-  while(my $seq = $fastx->next_seq){
-    open_file("$prefix" . $seq->header . "$suffix");
-    $seq->print(fh => $OUT, width => $width);
+
+  #print the rest
+  $itr++;
+  if ( $count > $num ) {
+    print_array("$prefix$itr$suffix", \@seqs);
+  }
+}
+elsif ($amount) {
+  my @seqs;
+  my $size = 0;
+  while ( my $rec = $fastx->next_seq ) {
+    push @seqs, $rec;
+    $size += length( $rec->sequence );
+  }
+  my $split_size = $size / $amount;
+  my $cur_size   = 0;
+  my $cutoff     = 0;
+  my $itr        = 0;
+  my $prev_itr = 0;
+  for my $itr (0..$#seqs) {
+    if ( $cur_size >= $cutoff ) {
+      print_array("$prefix$itr$suffix", \@seqs[$prev_itr..$itr]);
+      $prev_itr = $itr+1;
+      $cutoff += $split_size;
+    }
+    $cur_size += length( $seqs[$itr]->sequence );
+  }
+  print_array("$prefix$itr$suffix", \@seqs[$prev_itr..$#seqs]) if $prev_itr < $#seqs;
+}
+else {
+  while ( my $seq = $fastx->next_seq ) {
+    print_array("$prefix" . $seq->header . "$suffix", [ $seq ] );
   }
 }
 
-sub open_file{
-  my $filename = shift;
+sub print_array{
+  my ($filename, $seq_ref) = @_;
   die "$filename exists, use -force to overwrite" if -e $filename and not $force;
-  open $OUT, ">$filename" or die "$!: Could not open $filename";
+  open $OUT, ">$filename";
+  for my $seq(@{ $seq_ref }){
+    $seq->print( fh => $OUT, width => $width );
+  }
+  close $OUT;
+  #autoflush output
+  $|=1;
   print "$filename\n";
 }
 
-
+sub remove_suffix{
+  my ($filename) = @_;
+  $filename =~ s/[.] [^.]* $//x;
+  return $filename;
+}
 ###############################################################################
 # Help Documentation
 ###############################################################################
@@ -113,9 +158,13 @@ Options:
 
 force overwriting of existing files
 
+=item B<-amount>
+
+the amount of files to split the input into, default is one file per record
+
 =item B<-num>
 
-the number of files to split the input into, default is one file per record
+the number of records per file
 
 =item B<-prefix>
 
