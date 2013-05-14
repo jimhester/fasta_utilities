df3b12794f5ebd3c082840698b5187b7f501a9e9
Jim Hester <james.f.hester@gmail.com>
2013-05-14 08:57:24 -0400

    Add phred quality trimming to trim_fasta

--- scripts/trim_fasta.pl
+++ scripts/trim_fasta.pl
@@ -18,12 +18,14 @@ my $help     = 0;
 my $amount   = 36;
 my $startPos = 0;
 my $bwa;
+my @mott;
 my $random;
 
 GetOptions('num|trim|amount|t|a|n=i' => \$amount,
            'start=i'                 => \$startPos,
            'random|r'                => \$random,
-           'bwa-quality:i'             => \$bwa,
+           'bwa:i'             => \$bwa,
+           'mott:f{,2}'            => \@mott,
            'help|?'                  => \$help,
            man                       => \$man)
   or pod2usage(2);
@@ -36,7 +38,19 @@ pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 # trim_fasta.pl
 ###############################################################################
 
-$bwa = 20 if $bwa == 0;
+my @probs;
+my @mott_defaults = (0.05, 30);
+if(@mott){
+  @mott = mott_defaults(\@mott);
+  use Data::Dumper;
+  warn Dumper(@mott);
+  #phred scores from 33 to 128
+  @probs = map { 10**(-$_/10) } 0..98;
+}
+
+
+
+#$bwa = 20 if $bwa == 0;
 
 use ReadFastx;
 
@@ -46,6 +60,11 @@ while (my $seq = $file->next_seq) {
   if($bwa){
     $startPos = 0;
     $amount = bwa_cut($seq);
+  }
+  elsif(@mott){
+    my $end;
+    ($startPos, $end) = mott($seq, @mott);
+    $amount = $end - $startPos - 1;
   }
   elsif ($random) {
     $startPos = int(rand(length($seq->sequence) - $amount));
@@ -84,8 +103,59 @@ sub bwa_cut{
     last if $sum < 0;
     $itr--;
   }
-  warn $good_amount;
   return $good_amount;
+}
+
+sub mott{
+  my($sequence, $cutoff, $min_length) = @_;
+  my $qualities = $sequence->quality_array();
+
+  my($begin, $temp, $end, $sum, $max) = (0) x 5;
+
+  if(@{ $qualities } > $min_length){
+    for my $itr(0..$#{ $qualities }){
+      $sum += $cutoff - $probs[$qualities->[$itr]];
+      if($sum > $max){
+        $max = $sum;
+        $begin = $temp;
+        $end = $itr + 1;
+      }
+      if($sum < 0){
+        $sum = 0;
+        $temp = $itr + 1;
+      }
+    }
+    if($end - $begin < $min_length){
+      my($is, $imax, $itr);
+      for $itr(0..($min_length-1)){
+        $is += $qualities->[$itr];
+      }
+      $imax = $is;
+      $begin = 0;
+      for $itr($itr..$#{ $qualities} ){
+        $is += $qualities->[$itr] - $qualities->[$itr - $min_length];
+        if($imax < $is){
+          $imax = $is;
+          $begin = $itr - $min_length + 1;
+        }
+      }
+      $end = $begin + $min_length;
+    }
+  }
+  else {
+    $begin = 0;
+    $end = @{ $qualities };
+  }
+  return($begin, $end);
+}
+#sets mott defaults if they are not given
+sub mott_defaults {
+  my($val_ref) = @_;
+
+  #return if the mott argument is not used
+  return undef unless @{ $val_ref };
+
+  return map { $val_ref->[$_] ? $val_ref->[$_] : $mott_defaults[$_] } 0..1;
 }
 ###############################################################################
 # Help Documentation
@@ -102,6 +172,8 @@ trim_fasta.pl [options] [file ...]
 
 Options:
       -t,-n,-a,-trim,-num,-amount
+      -bwa
+      -mott
       -help
       -man               for more info
 
@@ -109,9 +181,17 @@ Options:
 
 =over 8
 
+=item B<-bwa>
+
+Trim based on qualities using the 1 sided algorithm used in bwa.
+
+=item B<-mott>
+
+Trim based on qualities using the 2 sided modified Richard Mott's algorithm used in Phred.
+
 =item B<-t,-n,-a,-trim,-num,-amount>
 
-Set the amount to trim
+Trim a set number of bases.
 
 =item B<-help>
 
